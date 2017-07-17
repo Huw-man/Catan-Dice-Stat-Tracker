39d2e02c8e76ff1f4ad12c3fe6a3f73a10ef6821
Huw-Man <hu.newman@berkeley.edu>
2017-07-17 14:36:03 +0800

    tracks rolls by player

--- CatanDice/CatanDice2.py
+++ CatanDice/CatanDice2.py
@@ -28,8 +28,8 @@ class DiceRoller(tk.Frame):
 
     def roll(self):
         self.rollNum.set(random.randint(1,6)+random.randint(1,6))
-        self.barGraph.drawGraph(int(self.rollNum.get()))
         tn = self.trackTurn()
+        self.barGraph.drawGraph(int(self.rollNum.get()), tn[1])
         if tn[1] == 'royal blue' :
             self.turn.set('blue turn')
         else:
@@ -47,23 +47,32 @@ class DiceRoller(tk.Frame):
 class BarGraph:
     print("barGraph")
     def __init__(self, parent):
-        self.width = 800
-        self.height = 600
+        self.width = 500
+        self.height = 400
         self.parent = parent
         #Canvas for barGraph
         self.graph = tk.Canvas(parent, width= self.width, height = self.height, bg='light gray')
         self.graph.grid(row=0, column=1, rowspan=2)
 
         labelGap = self.width/11 -7 #scales placement of labels and graphs according to canvas
-        #each dictionary key holds an array containing the number, start position of the bar, roll times
-        self.labels = {2:['2', labelGap, 0], 3:['3', 2*labelGap, 0], 4:['4', 3*labelGap, 0],
-            5:['5', 4*labelGap, 0], 6:['6', 5*labelGap, 0], 7:['7', 6*labelGap, 0],
-            8:['8', 7*labelGap, 0], 9:['9', 8*labelGap, 0], 10:['10', 9*labelGap, 0],
-            11:['11', 10*labelGap, 0], 12:['12', 11*labelGap, 0]}
+        #each dictionary key holds an array containing the number, start position of the bar, rolltimes
+        self.labels = {2:[labelGap, {'red': 0, 'royal blue':0, 'white':0, 'orange':0, 'green':0, 'brown':0}],
+            3:[2*labelGap, {'red': 0, 'royal blue':0, 'white':0, 'orange':0, 'green':0, 'brown':0}],
+            4:[3*labelGap, {'red': 0, 'royal blue':0, 'white':0, 'orange':0, 'green':0, 'brown':0}],
+            5:[4*labelGap, {'red': 0, 'royal blue':0, 'white':0, 'orange':0, 'green':0, 'brown':0}],
+            6:[5*labelGap, {'red': 0, 'royal blue':0, 'white':0, 'orange':0, 'green':0, 'brown':0}],
+            7:[6*labelGap, {'red': 0, 'royal blue':0, 'white':0, 'orange':0, 'green':0, 'brown':0}],
+            8:[7*labelGap, {'red': 0, 'royal blue':0, 'white':0, 'orange':0, 'green':0, 'brown':0}],
+            9:[8*labelGap, {'red': 0, 'royal blue':0, 'white':0, 'orange':0, 'green':0, 'brown':0}],
+            10:[ 9*labelGap, {'red': 0, 'royal blue':0, 'white':0, 'orange':0, 'green':0, 'brown':0}],
+            11:[ 10*labelGap, {'red': 0, 'royal blue':0, 'white':0, 'orange':0, 'green':0, 'brown':0}],
+            12:[ 11*labelGap, {'red': 0, 'royal blue':0, 'white':0, 'orange':0, 'green':0, 'brown':0}]}
         self.drawLabels(self.graph)
 
-    def drawGraph(self, entry): #update graph
-        self.labels[entry][2] +=1
+    def drawGraph(self, entry, player): #update graph
+        print(str(entry) + " "+player)
+        self.labels[entry][1][player] +=1
+        #print(self.labels)
         self.graph.destroy()
         newGraph = tk.Canvas(self.parent, width= self.width, height = self.height, bg='light gray')
         self.drawLabels(newGraph)
@@ -74,27 +83,43 @@ class BarGraph:
 
     def drawLabels(self, canvas):
         for i in self.labels: #write labels
-            canvas.create_text(self.labels[i][1], self.height-30, anchor='nw', text=self.labels[i][0], font=("Times", 20, "bold"))
+            canvas.create_text(self.labels[i][0], self.height-30, anchor='nw', text=i, font=("Times", 20, "bold"))
 
     def drawBars(self, canvas): #draw bars on graph
         minHeight = self.height-30
         fullHeight = minHeight - 30
         if self.findMaxRolls()!=0:
             segmentHeight = fullHeight/self.findMaxRolls()
-        for c in self.labels:
-            canvas.create_rectangle(self.labels[c][1] -7, minHeight-(self.labels[c][2]*segmentHeight),
-                self.labels[c][1] +27, minHeight, fill='royal blue')
-            #labels indicating how many rolls
-            if self.labels[c][2] != 0:
-                canvas.create_text(self.labels[c][1] +3, minHeight-(self.labels[c][2]*segmentHeight), anchor='nw', fill='white', text=self.labels[c][2], font=("Times", 14))
-            else:
-                canvas.create_text(self.labels[c][1] +3, minHeight-(self.labels[c][2]*segmentHeight) -20, anchor='nw', fill='white', text=self.labels[c][2], font=("Times", 14))
 
-    def findMaxRolls(self):
+        for c in self.labels:
+            #print(c)
+            tmin = minHeight
+            for d in self.labels[c][1]:
+                #print(d + str(self.labels[c][1][d]))
+                tmax = tmin - (self.labels[c][1][d]*segmentHeight)
+                #print(tmax)
+                canvas.create_rectangle(self.labels[c][0] -7, tmax, self.labels[c][0] +27, tmin, fill=d)
+                tmin = tmax
+
+            # canvas.create_rectangle(self.labels[c][1] -7, minHeight-(self.labels[c][1]*segmentHeight),
+            #     self.labels[c][1] +27, minHeight, fill='royal blue')
+            #labels indicating how many rolls
+            # if self.labels[c][1] != 0:
+            #     canvas.create_text(self.labels[c][1] +3, minHeight-(self.labels[c][1]*segmentHeight), anchor='nw', fill='white', text=self.labels[c][1], font=("Times", 14))
+            # else:
+            #     canvas.create_text(self.labels[c][1] +3, minHeight-(self.labels[c][1]*segmentHeight) -20, anchor='nw', fill='white', text=self.labels[c][1], font=("Times", 14))
+
+    def findMaxRolls(self): #finds maximum rolls of one number
+        sumOfRolls = {} #holds list of rolls of each number
+        for r in self.labels:
+            tsum = 0
+            for s in self.labels[r][1]:
+                tsum += self.labels[r][1][s]
+            sumOfRolls[r] = tsum
         max = 0
-        for a in self.labels:
-            if self.labels[a][2] > max :
-                max = self.labels[a][2]
+        for a in sumOfRolls:
+            if sumOfRolls[a] > max :
+                max = sumOfRolls[a]
         return max
 
 class SetupGame(tk.Frame):
